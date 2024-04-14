283ddf071b37ffd0bc0842d6d05238dbdb1af609
fernang0 <fer.henriquez248@gmail.com>
2024-04-13 21:39:24 -0400

    Actividad 2.2, nuestro primer programa

---
+++ 2-2-2.py
@@ -0,0 +1,16 @@
+print("Bienvenido al mundo de la programacion");
+print("Para comenzar, ingresa tu nombre");
+nom = str(input());
+print("Bienvenido", nom);
+x = int(input("Ingresa x para resolver la ecuacion:"));
+print((x**2+3*x+1)/4);
+print("\n\n");
+nombre=str(input("Ingresa tu nombre: "));
+apellido=str(input("Ingresa tu apellido: "));
+rut=str(input("Ingresa tu rut(formato xx.xxx.xxx-x): "));
+correo=str(input("Ingresa tu correo electronico: "));
+tel=str(input("Ingresa tu numero telefonico: "));
+print("\n\n\n")
+print("NOMBRE:\t\t",nombre, apellido,"\nRUT:\t\t",rut,"\nCORREO:\t\t",correo,"\nTELEFONO:\t", tel)
+
+#aburrido
