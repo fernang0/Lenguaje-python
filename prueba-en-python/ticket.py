31b073ffa0722a4a750cc9af3ee7602031a46f98
fernang0 <fer.henriquez248@gmail.com>
2024-04-13 20:35:46 -0400

    Ejercicios de la prueba en python

---
+++ prueba-en-python/ticket.py
@@ -0,0 +1,10 @@
+edad = int(input("Ingrese su edad: "));
+if edad>10 and edad<=18:
+    print("El valor del ticket es $2000");
+elif edad >18 and edad<=65:
+    print("El valor del ticket es $1500");
+elif edad>65:
+    print("El valor del ticket es $1000");
+else:
+    print("El ticket es GRATISSS");
+    
