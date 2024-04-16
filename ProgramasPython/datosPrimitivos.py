6e92fb647622f31309b39333b70679067d5b3d3b
fernang0 <fer.henriquez248@gmail.com>
2024-04-16 19:48:19 -0400

    actualizacion ejercicio por profesor

--- ProgramasPython/datosPrimitivos.py
+++ ProgramasPython/datosPrimitivos.py
@@ -1,1 +1,11 @@
-Edad = int(input("Ingrese su edad :"))
+Edad = int(input("Ingrese su edad :"));
+variable_entera = int(input("Ingresa un numero entero: "));
+variable_decimal = float(input("Ingresa un numero decimal:"));
+variable_texto = str(input("Ingresa una cadena de texto: "));
+variable_booleana = bool(input("Ingresa un valor booleno: "));
+
+print(Edad);
+print(variable_entera);
+print(variable_decimal);
+print(variable_texto);
+print(variable_booleana);
