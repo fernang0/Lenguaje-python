4769ca204438f732106bfd9b9d0b9a47a1e8b75a
fernang0 <fer.henriquez248@gmail.com>
2024-04-17 19:33:20 -0400

    Se comenta el programa anterior y se agrega un ejemplo de variables booleanas

--- clase17-04.py
+++ clase17-04.py
@@ -1,19 +1,26 @@
 #Condiciones if
 
-tacos = 1;
-pizza = 2;
-humitas = 3;
-cazuelas = 4;
+# tacos = 1;
+# pizza = 2;
+# humitas = 3;
+# cazuelas = 4;
 
-op = int(input("Elija una opcion de comida: "));
+# op = int(input("Elija una opcion de comida: "));
 
-if op == (1):
-    print("Usted prefiere los tacos");
-elif op == (2):
-    print("Usted prefiere las pizzas");
-elif op == (3):
-    print("Usted prefiere las humitas");
-elif op == (4):
-    print("Usted prefiere las cazuelas");
+# if op == (1):
+#     print("Usted prefiere los tacos");
+# elif op == (2):
+#     print("Usted prefiere las pizzas");
+# elif op == (3):
+#     print("Usted prefiere las humitas");
+# elif op == (4):
+#     print("Usted prefiere las cazuelas");
+# else:
+#     print("Usted es un maldito regodion");
+
+ticket = True;
+
+if ticket:
+    print("Este ticket es valido");
 else:
-    print("Usted es un maldito regodion");
+    print("Este ticket caduco");
