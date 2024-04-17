f5d7ef1a96078a45456de9548a7a7b8718f0c197
fernang0 <fer.henriquez248@gmail.com>
2024-04-17 19:23:52 -0400

    Clase de programacion del dia 17-04-2024

---
+++ clase17-04.py
@@ -0,0 +1,19 @@
+#Condiciones if
+
+tacos = 1;
+pizza = 2;
+humitas = 3;
+cazuelas = 4;
+
+op = int(input("Elija una opcion de comida: "));
+
+if op == (1):
+    print("Usted prefiere los tacos");
+elif op == (2):
+    print("Usted prefiere las pizzas");
+elif op == (3):
+    print("Usted prefiere las humitas");
+elif op == (4):
+    print("Usted prefiere las cazuelas");
+else:
+    print("Usted es un maldito regodion");
