edeb231291f5ec17b35734e2c9a27e634c5e85df
fernang0 <166668753+fernang0@users.noreply.github.com>
2024-04-16 20:55:38 -0400

    Update num-mayor.py
    Corrección de variable en condicion

--- prueba-en-python/num-mayor.py
+++ prueba-en-python/num-mayor.py
@@ -2,7 +2,7 @@ num1 = int(input("Ingresa el primer numero: "));
 num2 = int(input("Ingresa el segundo numero: "));
 num3 = int(input("Ingresa el tercer numero: "));
 
-if (num1>2 and num1>num3):
+if (num1>num2 and num1>num3):
     print(num1, "es el numero mayor");
 elif (num2>num1 and num2>num3):
     print(num2, "es el numero mayor");
