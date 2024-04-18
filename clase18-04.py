2d21d021acbd625e0b0b646462d2163dd3a425d1
fernang0 <fer.henriquez248@gmail.com>
2024-04-18 19:56:04 -0400

    Clases de funciones dia 18 04 2024

--- clase18-04.py
+++ clase18-04.py
@@ -3,6 +3,13 @@
 def sumar(x,y):
     return (x + y);
 
+def area(x,y):
+    return (x*y);
+def perimetro(x,y):
+    return (2*x+2*y);
+
 x = int(input("Ingrese el primer numero: "));
 y = int(input("Ingresa el segundo numero: "));
-print(sumar(x,y));
+
+print("El area del rectangulo es: ", area(x,y));
+print("El perimetro del rectangulo es: ", perimetro(x,y));
