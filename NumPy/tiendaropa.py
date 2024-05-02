7c5dd2cdaadd2741eb2294441cf37da41c7ef68d
fernang0 <fer.henriquez248@gmail.com>
2024-05-02 16:28:38 -0400

    matrices

--- NumPy/tiendaropa.py
+++ NumPy/tiendaropa.py
@@ -1,4 +1,5 @@
 import numpy as np
+#Escribe un programa en Python que solicite al usuario la cantidad de cada tipo de prenda en cada talla y luego calcule el costo total de la compra.
 precios = np.array([[10,12,15], [20,25,30],[35,40,45]]);
 def mostrar_precios():
     prendas = ["Camiseta", "Pantalon", "Caqueta"];
