65328030aa2b6a607077f48bd2544f0a325cc3e3
fernang0 <fer.henriquez248@gmail.com>
2024-05-03 13:59:26 -0400

    tienda de ropa usando matrices

--- NumPy/tiendaropa.py
+++ NumPy/tiendaropa.py
@@ -1,15 +1,15 @@
 import numpy as np
 #Escribe un programa en Python que solicite al usuario la cantidad de cada tipo de prenda en cada talla y luego calcule el costo total de la compra.
 precios = np.array([[10,12,15], [20,25,30],[35,40,45]]);
-carrito = np.zeros((3,3))
+carrito = np.zeros((3,3));
 prendas = ["Camiseta", "Pantalon", "Chaqueta"];
-tallas = ["Small", "Medium", "Large"]
+tallas = ["Small", "Medium", "Large"];
 def mostrar_precios():
     x = 0;
     y = 0;
     for prenda in prendas:
         for talla in tallas:
-            print(prenda, talla, "=", "$",precios[x][y])
+            print(prenda, talla, "=", "$",precios[x][y]);
             y+=1;
         x+=1;y=0;
 def tallas():
@@ -19,26 +19,33 @@ Que talla:
 2)Medium
 3)Large
 0)Menu principal
-    """)
+    """);
 def camisetas():
     tallas();
     opcion = int(input("Que talla desea comprar: "));
+    match opcion:
+        case 1:
+        #pedir cantidad y ingresar la cantidad en la respectiva cordenada de matriz vacia para luego multiplicar matriz carrito por precios
+        case 2:
+        case 3:
+        case 0:
+            comprar();
 
 def pantalones():
     tallas();
-    opcion = int(input("Que talla desea comprar: "))
+    opcion = int(input("Que talla desea comprar: "));
 
 def chaquetas():
     tallas();
-    opcion = int(input("Que talla desea comprar: "))
+    opcion = int(input("Que talla desea comprar: "));
 
 def comprar():
     print("""
 1) Camisetas
 2) Pantalones
 3) Chaquetas
-""")
-    opcion = int(input("Que desea comprar: "))
+""");
+    opcion = int(input("Que desea comprar: "));
     match opcion:
         case 1:
             camisetas();
